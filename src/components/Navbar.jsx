f89cde37db05031172cd59fbee43149739a0b927
Alain <ark2784@gmail.com>
2022-11-06 12:17:09 -0500

    Axl name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -69,9 +69,9 @@ const Navbar = () => {
               alt="user-profile"
             />
             <p>
-              <span className="text-gray-400 text-14">Hi,</span>{' '}
+              <span className="text-gray-400 text-14">Hola</span>{' '}
               <span className="text-gray-400 font-bold ml-1 text-14">
-                Michael
+                Axel
               </span>
             </p>
             <MdKeyboardArrowDown className="text-gray-400 text-14" />
